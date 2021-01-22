38e9c50f749439bf389ed921ac584a15b4f3a381
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2021-01-22 18:59:01 -0300

    Add 1001.rs

---
+++ rust/1001.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let a: i32 = input.trim().parse().unwrap();
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+    let b: i32 = input.trim().parse().unwrap();
+    println!("X = {}", a + b);
+}
